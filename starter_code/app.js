e0a498dfe386218832d6fd511a402d43e1702dc7
Michael Roth <michael.franklin.roth@gmail.com>
2019-07-05 10:49:39 -0400

    express setup complete

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
+
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 
 
@@ -13,13 +16,32 @@ app.use(express.static(__dirname + '/public'));
 
 
 // setting the spotify-api goes here:
+const clientId = '6d07cfac048840f49487239e6c0a5a1a',
+  clientSecret = '9d659c2bfb544741a5bf48382e01e62f';
 
+const spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
+});
 
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
 
 // the routes go here:
+app.get('/', (req, res, next) => {
+  res.render('home')
+})
 
 
 
